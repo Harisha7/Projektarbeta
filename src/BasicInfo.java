5a08dec2fab2d34ea582c5fdbd5d51799ea9097e
nahidalamgh <60609619+nahidalamgh@users.noreply.github.com>
2022-09-29 11:24:37 +0200

    Create BasicInfo.java class which contains the basic information of the band, it has constructor and getter setter method.

---
+++ src/BasicInfo.java
@@ -0,0 +1,47 @@
+public class BasicInfo {
+    private String name;
+    private String genres;
+    private int NumberOfMembers;
+    private int EstablishYear;
+
+
+    public BasicInfo(String name, String genres, int numberOfMembers, int establishYear) {
+        this.name = name;
+        this.genres = genres;
+        this.NumberOfMembers = numberOfMembers;
+        this.EstablishYear = establishYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public int getNumberOfMembers() {
+        return NumberOfMembers;
+    }
+
+    public void setNumberOfMembers(int numberOfMembers) {
+        NumberOfMembers = numberOfMembers;
+    }
+
+    public int getEstablishYear() {
+        return EstablishYear;
+    }
+
+    public void setEstablishYear(int establishYear) {
+        EstablishYear = establishYear;
+    }
+
+}
