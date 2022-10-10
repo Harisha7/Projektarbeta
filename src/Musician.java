c9bd95dd9cfeae1443904dfa7b6753a0598d4569
theunitzeroone <hanway.tran@gmail.com>
2022-10-10 12:33:27 +0200

    Add files via upload

---
+++ src/Musician.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Musician {
+    
+    private String name;
+    private String infoText;
+    private int yearOfBirth;
+    
+    
+    ArrayList<Band> currentBands = new ArrayList<>();
+    
+    ArrayList<Band> oldBands = new ArrayList<>();
+    
+    HashMap<Album, String> soloAlbum = new HashMap<>();
+    
+    ArrayList<String> instruments = new ArrayList<>();
+    
+    
+    public Musician(String name, String infoText, int yearOfBirth){
+        this.name = name;
+        this.infoText = infoText;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+    public String getInfoText(){
+
+        return infoText;
+    }
+    public int getYearOfBirth(){
+
+        return yearOfBirth;
+    }
+     public ArrayList<Band> getCurrentBands(){
+         return currentBands;
+     }
+    
+    public void setCurrentBands(ArrayList<Band>currentBands){
+        this.currentBands = currentBands;
+    }
+    
+    public ArrayList<Band> getOldBands(){
+         return oldBands;
+     }
+    
+    public void setOldBands(ArrayList<Band>currentBands){
+        this.oldBands = oldBands;
+    }
+    
+    public HashMap<Album, String> getSoloAlbum(){
+        return soloAlbum;
+    }
+    
+    public void setSoloAlbum(HashMap<Album,String> soloAlbum){
+        this.soloAlbum = soloAlbum;
+    }
+    
+    public ArrayList<String> getInstruments(){
+         return instruments;
+     }
+    
+    public void setInstruments(ArrayList<String> instruments){
+        this.instruments = instruments;
+    }
+}
